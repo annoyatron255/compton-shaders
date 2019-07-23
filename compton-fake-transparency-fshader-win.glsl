d8aa3fb033199d6d476f7e6052c1296ccad8fe4b
Jack Sweeney <annoyatron255@gmail.com>
2019-07-23 18:05:10 -0500

    Initial commit

---
+++ compton-fake-transparency-fshader-win.glsl
@@ -0,0 +1,17 @@
+uniform float opacity;
+uniform bool invert_color;
+uniform sampler2D tex;
+
+void main() {
+	vec4 c = texture2D(tex, gl_TexCoord[0].xy);
+	if (!invert_color) { // Hack to allow compton exceptions
+		// Change the vec4 to your desired key color
+		vec4 vdiff = abs(vec4(0.0039, 0.0039, 0.0039, 1.0) - c); // #010101
+		float diff = max(max(max(vdiff.r, vdiff.g), vdiff.b), vdiff.a);
+		// Change the vec4 to your desired output color
+		if (diff < 0.001)
+			c = vec4(0.0, 0.0, 0.0, 0.890196); // #000000E3
+	}
+	c *= opacity;
+	gl_FragColor = c;
+}
